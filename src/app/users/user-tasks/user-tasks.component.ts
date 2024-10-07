44a3a284bb87f31449687cd20a0de907fed72e29
x404 <kronos2003@gmail.com>
2024-10-07 14:41:02 +0300

    feat: extracting route parameters  via Inputs

--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -1,4 +1,5 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
+import { UsersService } from "../users.service";
 
 @Component({
   selector: 'app-user-tasks',
@@ -8,4 +9,7 @@ import { Component, input } from '@angular/core';
 })
 export class UserTasksComponent {
   userId = input.required<string>();
+  private usersService = inject(UsersService);
+
+  userName = computed(() => this.usersService.users.find(u => u.id === this.userId())?.name);
 }
