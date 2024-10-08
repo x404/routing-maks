8ce6d9e064954136a2b14409082a67c2bb55417d
x404 <kronos2003@gmail.com>
2024-10-08 12:37:31 +0300

    feat: added redirect

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -15,6 +15,11 @@ export const routes: Routes = [
     component: UserTasksComponent,
     children: [
       {
+        path: '',
+        redirectTo: 'tasks',
+        pathMatch: 'prefix',
+      },
+      {
         path: 'tasks',
         component: TasksComponent,
       },
