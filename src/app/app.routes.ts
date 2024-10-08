359edbef612a8835fd18ac9a51de0d301f3d88f7
x404 <kronos2003@gmail.com>
2024-10-08 12:43:08 +0300

    refactor: reorganize users routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,8 @@
 import { Routes } from "@angular/router";
-import { TasksComponent } from "./tasks/tasks.component";
+import { routes as userRoutes } from "./users/users.routes";
+
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
-import { UserComponent } from "./users/user/user.component";
 import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
-import { NewTaskComponent } from "./tasks/new-task/new-task.component";
 
 export const routes: Routes = [
   {
@@ -13,25 +12,7 @@ export const routes: Routes = [
   {
     path: 'users/:userId',
     component: UserTasksComponent,
-    children: [
-      {
-        path: '',
-        redirectTo: 'tasks',
-        pathMatch: 'prefix',
-      },
-      {
-        path: 'tasks',
-        component: TasksComponent,
-      },
-      {
-        path: 'tasks/new',
-        component: NewTaskComponent
-      },
-      {
-        path: '**',
-        component: NoTaskComponent
-      }
-    ],
+    children: userRoutes,
   },
 //   {
 //   path: 'tasks',
---
+++ src/app/users/users.routes.ts
@@ -0,0 +1,25 @@
+import { Routes } from "@angular/router";
+
+import { TasksComponent } from "../tasks/tasks.component";
+import { NewTaskComponent } from "../tasks/new-task/new-task.component";
+import { NoTaskComponent } from "../tasks/no-task/no-task.component";
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'tasks',
+    pathMatch: 'prefix',
+  },
+  {
+    path: 'tasks',
+    component: TasksComponent,
+  },
+  {
+    path: 'tasks/new',
+    component: NewTaskComponent
+  },
+  {
+    path: '**',
+    component: NoTaskComponent
+  }
+]
